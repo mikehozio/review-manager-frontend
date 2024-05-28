f8ce65e076e342ef557e18b5d6f89d8db0be3dcc
Mike Horan <mike.horan06@gmail.com>
2024-05-28 16:59:01 -0400

    modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/App.css 	modified:   src/App.js 	new file:   src/SingleReview.js 	modified:   src/index.css 	new file:   src/tgkPkLn-blue-hello-kitty-wallpaper.jpg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,179 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import {Stack, Chip, MenuItem, FormControl, Select, Button, Tooltip, ButtonBase} from '@mui/material';
+import LoadingButton from '@mui/lab/LoadingButton';
+import SingleReview from './SingleReview';
+
+const RATING_TO_INT = {'FIVE': 5, 'FOUR': 4, 'THREE': 3, 'TWO': 2, 'ONE': 1}
 
 function App() {
+  const [sort, setSort] = React.useState('');
+  const locationsObj = React.useRef({});
+  const [locationsList, setLocationsList] = React.useState(null);
+  const [reviews, setReviews] = React.useState({});
+  const [reviewsList, setReviewsList] = React.useState([]);
+  const reviewPage = React.useRef(1);
+  const [activeLoc, setActiveLoc] = React.useState('*');
+  const [fetching, setFetching] = React.useState(false);
+  const [allClicked, setAllClicked] = React.useState(true);
+  const [repliedClicked, setRepliedClicked] = React.useState(false);
+  const [notRepliedClicked, setNotRepliedClicked] = React.useState(false);
+
+  React.useEffect(() => {
+    async function getData() {
+      await fetch('http://192.168.1.100:12500/all-locations').then(
+        res => res.json().then(
+          jsonres => {
+            locationsObj.current = jsonres;
+            var ordered = Object.keys(jsonres).map((gbp_id) => {
+              let i = jsonres[gbp_id]
+              i['gbp_id'] = gbp_id
+              return i
+            });
+            console.log(ordered);
+            ordered.sort((a,b) => (a.gbp_name.localeCompare(b.gbp_name)));
+            console.log(ordered);
+            setLocationsList(ordered);
+          }
+        )
+      );
+      await fetch('http://192.168.1.100:12500/get-reviews').then(
+        res => res.json().then(
+          jsonres => {
+            console.log(jsonres);
+            setReviews(jsonres);
+            setReviewsList([...reviewsList, ...jsonres['reviews']]);
+          }
+        )
+      );
+    }
+    getData()
+  }, []);
+
+
+  const handleFetchReviews = async () => {
+    setFetching(true);
+    await fetch(`http://192.168.1.100:12500/fetch-new`).then(
+      res => res.json().then(
+        jsonres => {
+          setReviews({'reviews': jsonres['reviews'], 'allReviews': jsonres['allReviews'], 'replied': jsonres['replied']});
+          setReviewsList(jsonres['reviews']);
+          locationsObj.current = jsonres['locations'];
+          var ordered = Object.keys(jsonres['locations']).map((gbp_id) => {
+            let i = jsonres['locations'][gbp_id]
+            i['gbp_id'] = gbp_id
+            return i
+          });
+          console.log(ordered);
+          ordered.sort((a,b) => (a.gbp_name.localeCompare(b.gbp_name)));
+          console.log(ordered);
+          setLocationsList(ordered);
+        }
+      )
+    );
+    setFetching(false);
+  }
+
+  const handleLoadMore = async () => {
+    reviewPage.current = reviewPage.current + 1;
+    await fetch(`http://192.168.1.100:12500/get-reviews?gbpid=${activeLoc}&page=${reviewPage.current}`).then(
+      res => res.json().then(
+        jsonres => {
+          console.log({'reviews': [...reviews['reviews'], ...jsonres['reviews']], 'allReviews': jsonres['allReviews'], 'replied': jsonres['replied']});
+          setReviews({'reviews': [...reviews['reviews'], ...jsonres['reviews']], 'allReviews': jsonres['allReviews'], 'replied': jsonres['replied']});
+          setReviewsList([...reviews['reviews'], ...jsonres['reviews']]);
+        }
+      )
+    );
+  }
+
+  const handleAccountClick = async (loc) => {
+    reviewPage.current = 1;
+    console.log(loc);
+    activeLoc === loc.gbp_id?setActiveLoc('*'):setActiveLoc(loc.gbp_id);
+    await fetch(`http://192.168.1.100:12500/get-reviews?gbpid=${activeLoc === loc.gbp_id?'*':loc.gbp_id}`).then(
+      res => res.json().then(
+        jsonres => {
+          console.log(jsonres);
+          setReviews(jsonres);
+          setReviewsList(jsonres['reviews']);
+        }
+      )
+    );
+    window.scrollTo(0, 0);
+  }
+
+  const handleFilterReviews = (filter) => {
+    console.log(filter);
+    console.log(reviews['reviews']);
+    setReviewsList(filter === 'all'?reviews['reviews']:filter === 'replied'?reviews['reviews'].filter(review => 'reviewReply' in review):reviews['reviews'].filter(review => !('reviewReply' in review)));
+    console.log(reviews['reviews']);
+  }
+
+  const sortReviews = (event) => {
+    console.log(event.target.value);
+    setSort(event.target.value);
+    var sorted = [...reviewsList];
+    setReviewsList(event.target.value === ''?sorted:event.target.value === 'lowest'?sorted.sort((a,b) => (RATING_TO_INT[a.starRating] - RATING_TO_INT[b.starRating])):sorted.sort((a,b) => (RATING_TO_INT[b.starRating] - RATING_TO_INT[a.starRating])));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Stack direction="row" className='page-wrapper' alignItems='flex-start' justifyContent='center' spacing={3}>
+        <div className="sidebar sticky">
+          <h1 className="sidebar-label">Accounts</h1>
+          <div className="sidebar-accounts-list-wrapper">
+            <Stack className='accounts-list' spacing={1} alignItems='flex-start' >
+              {locationsList && locationsList.map((loc, i) =>
+                (<ButtonBase className={activeLoc === loc.gbp_id?'account-wrap-wrapper-clickable-active':'account-wrap-wrapper-clickable'} onClick={() => handleAccountClick(loc)} key={i}>
+                  <Tooltip title={`${loc.gbp_name} (${loc.address})`}>
+                    <Stack direction="row" alignItems='center'className='account-wrap' spacing={1}>
+                      <div className="account-name-wrap"><span className="account-name">{`${loc.gbp_name} (${loc.address})`}</span></div>
+                      <Chip label={'totalReviewCount' in loc?(loc.totalReviewCount):(0)} color="info"/>
+                      <Chip label={('averageRating' in loc)?((loc.averageRating).toFixed(1)):('0.0')} className='overall-rating'/>
+                    </Stack>
+                  </Tooltip>
+                </ButtonBase>
+              ))}
+            </Stack>
+          </div>
+        </div>
+        <div className="reviews-content">
+          <div className="reviews-menu-filters">
+            <Stack direction="row" spacing={2} alignItems='flex-start'>
+              <Chip label={`All (${reviews['allReviews']})`} color={allClicked?"info":"default"} onClick={() => {setAllClicked(true);setRepliedClicked(false);setNotRepliedClicked(false);handleFilterReviews('all')}}/>
+              <Chip label={`Replied (${reviews['replied']})`} color={repliedClicked?"info":"default"} onClick={() => {setAllClicked(false);setRepliedClicked(true);setNotRepliedClicked(false);handleFilterReviews('replied')}}/>
+              <Chip label={`Haven't Replied (${reviews['allReviews'] - reviews['replied']})`} color={notRepliedClicked?"info":"default"} onClick={() => {setAllClicked(false);setRepliedClicked(false);setNotRepliedClicked(true);handleFilterReviews('notreplied')}}/>
+              <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+                <Select
+                  id="demo-select-small"
+                  value={sort}
+                  displayEmpty={true}
+                  onChange={sortReviews}
+                  inputProps={{ 'aria-label': 'Without label' }}
+                >
+                  <MenuItem value="">
+                    <em>Newest</em>
+                  </MenuItem>
+                  <MenuItem value={'lowest'}>Lowest</MenuItem>
+                  <MenuItem value={'highest'}>Highest</MenuItem>
+                </Select>
+              </FormControl>
+              <Stack className='fetchbutton-wrap' spacing={0} alignItems='flex-end'>
+                <LoadingButton variant="contained" loading={fetching} onClick={handleFetchReviews} loadingIndicator="Fetching...">Fetch Latest Reviews</LoadingButton>
+              </Stack>
+            </Stack>
+          </div>
+          <div className="all-reviews">
+            <Stack className="all-reviews-wrapper" spacing={2}>
+              {reviewsList && reviewsList.map((review) => (
+                <SingleReview review={review} locationsObj={locationsObj} key={review.reviewId}/>
+              ))}
+              <Button className="load-more-button" variant="contained" color='info' onClick={handleLoadMore}>Load More</Button>
+            </Stack>
+          </div>
+        </div>
+      </Stack>
     </div>
   );
 }
---
+++ src/SingleReview.js
@@ -0,0 +1,84 @@
+import {Stack, Button} from '@mui/material';
+import StarIcon from '@mui/icons-material/Star';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+import CreateOutlinedIcon from '@mui/icons-material/CreateOutlined';
+import { useState } from 'react';
+const RATING_TO_INT = {'FIVE': 5, 'FOUR': 4, 'THREE': 3, 'TWO': 2, 'ONE': 1}
+
+function SingleReview ({ review, locationsObj }) {
+    const [replying, setReplying] = useState(false);
+    const [replyValue, setReplyValue] = useState('reviewReply' in review?review.reviewReply.comment:'');
+    const [submitColor, setSubmitColor] = useState('primary')
+
+    const handleSubmit = async () => {
+        console.log(replyValue);
+        const rawResponse = await fetch('http://192.168.1.100:12500/review-reply', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({reviewName: review.name, reply: replyValue})
+        });
+        console.log(rawResponse);
+        if (rawResponse.ok) {
+            setSubmitColor('success');
+            setReplying(false);
+            console.log(rawResponse.status);
+        } else {
+            setSubmitColor('error');
+            console.log(rawResponse.status);
+        }
+    }
+
+    const handleSuggestResponse = async () => {
+        console.log(replyValue);
+        setReplying(true);
+        const rawResponse = await fetch('http://192.168.1.100:12500/suggest-response', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({reviewContent: review.comment})
+        });
+        console.log(rawResponse);
+        if (rawResponse.ok) {
+            const res = await rawResponse.json()
+            setReplyValue(res['reviewResponse']);
+            console.log(res);
+        } else {
+            console.log(rawResponse.status);
+        }
+    }
+
+    return (
+        <div className="review-wrapper" key={review.reviewId}>
+            <Stack className="review-header" direction='row' justifyContent='space-between'>
+                <Stack className="reviewer-info-wrapper" direction='row'>
+                    <img src={review.reviewer.profilePhotoUrl} className="user-img" style={{borderRadius: '50%', height: '40px', width: '40px'}}/>
+                    <Stack className="reviewer-name-stars-gmbname">
+                        <Stack className="reviewer-stars" direction='row'>
+                            <div className="reviewer-name-wrap"><span className="reviewer-name">{review.reviewer.displayName}</span></div>
+                            <Stack className="stars" direction='row' spacing={0}>{Array(RATING_TO_INT[review.starRating]).fill(<StarIcon fontSize="small"/>)}{Array(5-RATING_TO_INT[review.starRating]).fill(<StarBorderIcon fontSize="small"/>)}</Stack>
+                        </Stack>
+                    <div className="location-name-review"><span className="location-name">{locationsObj.current[review.name.split('/review')[0].split('locations/').slice(-1)[0]]['gbp_name']}</span></div>
+                    </Stack>
+                </Stack>
+                <div className="review-date-wrap"><span className="raview-date">{new Date(review.createTime.split('T')[0]).toLocaleDateString()}</span></div>
+            </Stack>
+            <div className="review-content-wrap"><span className="review-content">{review.comment}</span></div>
+            {!replying && replyValue !== '' && <div className="review-response-wrap">
+                <span className="review-response-content">{replyValue}</span>
+            </div>}
+            {replying && <div className='review-reply-editor'><textarea placeholder='Enter your reply here' rows='6' className='review-textarea' value={replyValue} onChange={(e) => setReplyValue(e.target.value)}></textarea></div>}
+            <Stack className="review-footer" direction='row' justifyContent='flex-end'>
+                {(replying || (!replying && !('reviewReply' in review))) && <Button className="suggest-button" onClick={() => handleSuggestResponse()}>Suggest Reply</Button>}
+                {replying && <Button className="cancel-button" onClick={() => {setReplying(!replying); setReplyValue('');}}>cancel</Button>}
+                {replying?<Button className="submit-button" variant="contained" color={submitColor} onClick={handleSubmit}>Submit</Button>:<Button className="reply-button" variant="contained" onClick={() => setReplying(true)} endIcon={<CreateOutlinedIcon />}>{'reviewReply' in review?'Edit Reply':'Reply'}</Button>}
+            </Stack>
+        </div>
+    )
+}
+
+export default SingleReview
